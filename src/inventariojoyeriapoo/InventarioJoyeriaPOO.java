e80f4246927b2e291a591bcac4e704e721c31d66
Sara Reyna <shayty.reyna@gmail.com>
2023-01-12 18:13:33 -0600

    Iniciar Sesion

---
+++ src/inventariojoyeriapoo/InventarioJoyeriaPOO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventariojoyeriapoo;
+
+/**
+ *
+ * @author Sara.Reyna
+ */
+public class InventarioJoyeriaPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //PRuebaFrame FM = new PRuebaFrame();
+        
+        //FM.setVisible(true);
+        InicioSesion IS = new InicioSesion();
+        
+        
+        IS.setVisible(true);
+    }
+    
+    
+    
+}
