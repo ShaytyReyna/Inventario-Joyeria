4604a8bd02c754605edbf90b2daf44c705aeb484
Sara Reyna <shayty.reyna@gmail.com>
2023-01-14 14:51:13 -0600

    Crear clases de Inventario, Pedidos y Compras
    Cree las clases del diagrama que acordamos, pero aun sin el atributo de fecha

---
+++ src/inventariojoyeriapoo/Compras.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventariojoyeriapoo;
+
+/**
+ *
+ * @author Sara.Reyna
+ */
+public class Compras {
+    public int ID_C;
+    public int ID_Pr;
+    public int Cantidad;
+    //DATE
+    //public DATE Fecha;
+    
+    //constructor
+    public Compras(){
+        
+    }
+    //Metodos
+    public void AgregarProducto(){
+        
+    }
+    public void ModificarPRoducto(){
+        
+    }
+    
+}
---
+++ src/inventariojoyeriapoo/PedidosClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventariojoyeriapoo;
+
+/**
+ *
+ * @author Sara.Reyna
+ */
+public class PedidosClass {
+    public int ID_Pe;
+    public String Nom_Client;
+    //si ya no vamos a manejar distintos productos entonces quito el arreglo?
+    public int ID_Pr;
+    public int Cantidad;
+    //public DATE Fecha;
+    public void AgregarPedido(){
+        
+    }
+    public void CompletarPedido(){
+        
+    }
+    public void CancelarPedido(){
+        
+    }
+}
